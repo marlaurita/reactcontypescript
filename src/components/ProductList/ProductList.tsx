6b3ef669cd49bbf8674582dfa75537c13af13967
Maria Laura <10726179+marlaurita@users.noreply.github.com>
2026-06-25 19:36:10 -0400

    feat: search product

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -8,6 +8,7 @@ function ProductList() {
     const [products, setProducts] = useState<Product[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [search, setSearch] = useState("");
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -24,17 +25,29 @@ function ProductList() {
             })
     }, [])
 
+    const filtered = products.filter((p) => p.title.toLowerCase().includes(search.toLocaleLowerCase()) || 
+                                            p.description.toLowerCase().includes(search.toLocaleLowerCase()) )
+
     if (loading) return <p>Cargando productos</p>
     if (error) return <p>Hubo un error {error}</p>
 
     return (
         <section className="product-list">
             <h2 className="product-list__title">Productos</h2>
+            <input type="text" 
+            className="product-list__search"
+            placeholder="Buscar productos..." 
+            value={search} 
+            onChange={(e) => setSearch(e.target.value)}/>
             <div className="product-list__grid">
-                {products.map((product) => (
+                {filtered.map((product) => (
                     <ProductCard key={product.id} product={product} onClick={() => navigate(`${product.id}`)} />
                 ))}
             </div>
+
+            {filtered.length=== 0 && (
+                <p className="product-list__status"> No se encontraron resultados</p>
+            )}
         </section>
 
     )
